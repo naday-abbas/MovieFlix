6a48590a7d80d218cfe4c4c0fbc5c7627abb38dd
Naday Abbas <nadayabbas786@gmail.com>
2025-02-28 23:44:55 +0500

    Add files via upload

---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+const API_KEY = "2c91cfded1687746bbf8b1d21b016537";
+const BASE_URL = "https://api.themoviedb.org/3"
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json()
+    return data.results
+}
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json()
+    return data.results
+}
